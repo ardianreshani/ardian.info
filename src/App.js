f5980553221f9fa3a2c9b35f6637164166d03c32
aRdiiaNaRdiiaN04 <youngcreative12@gmail.com>
2022-01-05 10:09:45 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import "./App.css";
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-  motion,
   useMotionValue,
-  useSpring,
+  // useSpring,
   AnimatePresence,
 } from "framer-motion";
 
-import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Work from "./pages/Work";
-import About from "./pages/About";
+// import Home from "./pages/Home";
+// import Navbar from "./components/Navbar";
+// import Work from "./pages/Work";
+// import About from "./pages/About";
 import Loader from "./components/Loader";
 
 function App() {
-  const [loading, setLoading] = React.useState(false);
+  // const [loading, setLoading] = React.useState(false);
   const cursorX = useMotionValue(-100);
   const cursorY = useMotionValue(-100);
-  const springConfig = { damping: 25, stiffness: 700 };
-  const cursorXSpring = useSpring(cursorX, springConfig);
-  const cursorYSpring = useSpring(cursorY, springConfig);
+  // const springConfig = { damping: 25, stiffness: 700 };
+  // const cursorXSpring = useSpring(cursorX, springConfig);
+  // const cursorYSpring = useSpring(cursorY, springConfig);
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
+    // setLoading(true);
+    // setTimeout(() => {
+    //   setLoading(false);
+    // }, 5000);
     const moveCursor = (e) => {
       cursorX.set(e.clientX - 16);
       cursorY.set(e.clientY - 16);
@@ -35,7 +34,7 @@ function App() {
     return () => {
       window.removeEventListener("mousemove", moveCursor);
     };
-  }, [cursorX, cursorY, setLoading]);
+  }, [cursorX, cursorY]);
 
   return (
     <>
